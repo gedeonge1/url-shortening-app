4c9be15a048c7e5c21cbbe3ffd298fc615d7e906
gedeonge1 <136656685+gedeonge1@users.noreply.github.com>
2023-08-13 19:50:00 +0200

    I started working on js by getting all necessary elements to display short link and handle any error

---
+++ script.js
@@ -0,0 +1,5 @@
+let longLink = document.querySelector('#long-link')
+let shortenBtn = document.querySelector('#shorten-btn')
+let generatedLink = document.querySelector('#generated-link')
+let copyBtn = document.querySelector('.copy')
+let emptyInputText = document.querySelector('.handle-empty-input')

595c8a8066d0c079be68a5a77a2fd541ecace2c2
gedeonge1 <136656685+gedeonge1@users.noreply.github.com>
2023-08-13 19:58:25 +0200

    typo error is has  been corrected

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ let shortenBtn = document.querySelector('#shorten-btn')
 let generatedLink = document.querySelector('#generated-link')
 let copyBtn = document.querySelector('.copy')
 let emptyInputText = document.querySelector('.handle-empty-input')
+
+async function fetchShortLink(){
+
+}

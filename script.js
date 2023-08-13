ee91e9356cbc5d8e38f2909afb21fe5bda6bbbb5
gedeonge1 <136656685+gedeonge1@users.noreply.github.com>
2023-08-13 20:10:53 +0200

    I have fetched data from api, I converted them into json and then displayed generated link on the page. Moreover to that, I even handled all errors if link provided long link is invalid or empty

--- script.js
+++ script.js
@@ -5,5 +5,15 @@ let copyBtn = document.querySelector('.copy')
 let emptyInputText = document.querySelector('.handle-empty-input')
 
 async function fetchShortLink(){
+    const data = await fetch(`https://api.shrtco.de/v2/shorten?url=${longLink.value}`)
 
+    if(!data.ok){
+        emptyInputText.style.display = 'block'
+    } else{ 
+        emptyInputText.style.display = 'none'
+        const jsonResult = await data.json()
+        const  newLink = jsonResult.result.short_link2
+        generatedLink.value = newLink 
+   }    
 }
+

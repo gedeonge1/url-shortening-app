2df992f12c9a064ce9ea058cb4fe0649639bd025
gedeonge1 <136656685+gedeonge1@users.noreply.github.com>
2023-08-13 20:48:24 +0200

    I removed red borders around the result box and the result box will be displayed only if there is a new generated short link

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ let shortenBtn = document.querySelector('#shorten-btn')
 let generatedLink = document.querySelector('#generated-link')
 let copyBtn = document.querySelector('.copy')
 let emptyInputText = document.querySelector('.handle-empty-input')
+let resultBox = document.querySelector('.result-box')
 
 async function fetchShortLink(){
     const data = await fetch(`https://api.shrtco.de/v2/shorten?url=${longLink.value}`)
-
+    longLink.value = ''
     try{
     if(!data.ok){
         emptyInputText.style.display = 'block'
+        resultBox.style.display = 'none'
     } else{ 
         emptyInputText.style.display = 'none'
+        resultBox.style.display = 'flex'
         const jsonResult = await data.json()
         const  newLink = jsonResult.result.short_link2
         generatedLink.value = newLink 
